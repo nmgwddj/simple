2b2ebe3ca7d932d38078438b09ef3ab6c7bd2550
Dylan <2894220@gmail.com>
2021-07-15 11:50:31 +0800

    feat: add query function provider jieba query option
    Signed-off-by: Dylan <2894220@gmail.com>

--- src/entry.cc
+++ src/entry.cc
@@ -1,26 +1,49 @@
-#include "simple_highlight.h"
-#include "simple_tokenizer.h"
-SQLITE_EXTENSION_INIT1
+/**
+ * @file entry.cc
+ * @author github @wangfenjin
+ * @brief
+ * @version 0.1
+ * @date 2021-07-15
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 
 #include <cstring>
 #include <new>
+#include <string>
+#include "simple_def.h"
+#include "simple_highlight.h"
+#include "simple_tokenizer.h"
 
-int fts5_simple_xCreate(void *sqlite3, const char **azArg, int nArg, Fts5Tokenizer **ppOut) {
-  int rc = SQLITE_OK;
-  simple_tokenizer::SimpleTokenizer *p = new simple_tokenizer::SimpleTokenizer(azArg, nArg);
-  *ppOut = reinterpret_cast<Fts5Tokenizer *>(p);
-  return rc;
+SQLITE_EXTENSION_INIT1
+
+int fts5_simple_xCreate(void* sqlite3,
+                        const char** azArg,
+                        int nArg,
+                        Fts5Tokenizer** ppOut) {
+    int rc = SQLITE_OK;
+    simple_tokenizer::SimpleTokenizer* p =
+        new simple_tokenizer::SimpleTokenizer(azArg, nArg);
+    *ppOut = reinterpret_cast<Fts5Tokenizer*>(p);
+    return rc;
 }
 
-int fts5_simple_xTokenize(Fts5Tokenizer *tokenizer_ptr, void *pCtx, int flags, const char *pText, int nText,
+int fts5_simple_xTokenize(Fts5Tokenizer* tokenizer_ptr,
+                          void* pCtx,
+                          int flags,
+                          const char* pText,
+                          int nText,
                           xTokenFn xToken) {
-  simple_tokenizer::SimpleTokenizer *p = (simple_tokenizer::SimpleTokenizer *)tokenizer_ptr;
-  return p->tokenize(pCtx, flags, pText, nText, xToken);
+    simple_tokenizer::SimpleTokenizer* p =
+        (simple_tokenizer::SimpleTokenizer*)tokenizer_ptr;
+    return p->tokenize(pCtx, flags, pText, nText, xToken);
 }
 
-void fts5_simple_xDelete(Fts5Tokenizer *p) {
-  simple_tokenizer::SimpleTokenizer *pST = (simple_tokenizer::SimpleTokenizer *)p;
-  delete (pST);
+void fts5_simple_xDelete(Fts5Tokenizer* p) {
+    simple_tokenizer::SimpleTokenizer* pST =
+        (simple_tokenizer::SimpleTokenizer*)p;
+    delete (pST);
 }
 
 /*
@@ -28,103 +51,185 @@ void fts5_simple_xDelete(Fts5Tokenizer *p) {
 ** If an error occurs, return NULL and leave an error in the database
 ** handle (accessible using sqlite3_errcode()/errmsg()).
 */
-static int fts5_api_from_db(sqlite3 *db, fts5_api **ppApi) {
-  sqlite3_stmt *pStmt = 0;
-  int rc;
+static int fts5_api_from_db(sqlite3* db, fts5_api** ppApi) {
+    sqlite3_stmt* pStmt = 0;
+    int rc;
 
-  *ppApi = 0;
-  rc = sqlite3_prepare(db, "SELECT fts5(?1)", -1, &pStmt, 0);
-  if (rc == SQLITE_OK) {
-    sqlite3_bind_pointer(pStmt, 1, reinterpret_cast<void *>(ppApi), "fts5_api_ptr", 0);
-    (void)sqlite3_step(pStmt);
-    rc = sqlite3_finalize(pStmt);
-  }
+    *ppApi = 0;
+    rc = sqlite3_prepare(db, "SELECT fts5(?1)", -1, &pStmt, 0);
+    if (rc == SQLITE_OK) {
+        sqlite3_bind_pointer(pStmt, 1, reinterpret_cast<void*>(ppApi),
+                             "fts5_api_ptr", 0);
+        (void)sqlite3_step(pStmt);
+        rc = sqlite3_finalize(pStmt);
+    }
 
-  return rc;
+    return rc;
 }
 
 #ifdef USE_JIEBA
-static void jieba_dict(sqlite3_context *pCtx, int nVal, sqlite3_value **apVal) {
-  int rc;
-  if (nVal >= 1) {
-    const char *text = (const char *)sqlite3_value_text(apVal[0]);
-    if (text) {
-      std::string tmp(text);
-      char sep = '/';
+static void jieba_dict(sqlite3_context* pCtx, int nVal, sqlite3_value** apVal) {
+    if (nVal >= 1) {
+        const char* text = (const char*)sqlite3_value_text(apVal[0]);
+        if (text) {
+            std::string tmp(text);
+            char sep = '/';
 #ifdef _WIN32
-      sep = '\\';
+            sep = '\\';
 #endif
-      if (tmp.back() != sep) {  // Need to add a
-        tmp += sep;             // path separator
-      }
-      simple_tokenizer::jieba_dict_path = tmp;
-      sqlite3_result_text(pCtx, tmp.c_str(), -1, SQLITE_TRANSIENT);
-      return;
+            if (tmp.back() != sep) {  // Need to add a
+                tmp += sep;           // path separator
+            }
+            simple_tokenizer::jieba_dict_path__ = tmp;
+            sqlite3_result_text(pCtx, tmp.c_str(), -1, SQLITE_TRANSIENT);
+            return;
+        }
     }
-  }
-  sqlite3_result_null(pCtx);
+    sqlite3_result_null(pCtx);
 }
 
-static void jieba_query(sqlite3_context *pCtx, int nVal, sqlite3_value **apVal) {
-  int rc;
-  if (nVal >= 1) {
-    const char *text = (const char *)sqlite3_value_text(apVal[0]);
-    if (text) {
-      int flags = 1;
-      if (nVal >= 2) {
-        flags = atoi((const char *)sqlite3_value_text(apVal[1]));
-      }
-      std::string result = simple_tokenizer::SimpleTokenizer::tokenize_jieba_query(text, std::strlen(text), flags);
-      sqlite3_result_text(pCtx, result.c_str(), -1, SQLITE_TRANSIENT);
-      return;
+static void query(sqlite3_context* pCtx, int nVal, sqlite3_value** apVal) {
+    if (nVal < 1) {
+        sqlite3_result_null(pCtx);
+        return;
     }
-  }
-  sqlite3_result_null(pCtx);
+
+    // search keywords
+    auto keywords = (const char*)sqlite3_value_text(apVal[0]);
+
+    // query option
+    QueryOption query_option = kDefault;
+    if (nVal >= 2) {
+        auto argument = (const char*)sqlite3_value_text(apVal[1]);
+        query_option = (QueryOption)std::atoi(argument);
+    }
+
+    // pingyin option
+    bool using_pinyin = false;
+    if (nVal >= 3) {
+        auto argument = (const char*)sqlite3_value_text(apVal[2]);
+        using_pinyin = std::atoi(argument) > 0;
+    }
+
+    std::string result;
+
+    switch (query_option) {
+        case kDefault:
+            result = keywords;
+            break;
+        case kSimple:
+            result = simple_tokenizer::SimpleTokenizer::tokenize_query(
+                keywords, std::strlen(keywords), using_pinyin ? 1 : 0);
+            break;
+        case kJiebaCutWithHMM:
+        case kJiebaCutWithoutHMM:
+        case kJiebaCutAll:
+        case kJiebaCutForSearch:
+            result = simple_tokenizer::SimpleTokenizer::tokenize_jieba_query(
+                keywords, std::strlen(keywords), using_pinyin ? 1 : 0,
+                query_option);
+            break;
+        default:
+            break;
+    }
+
+    sqlite3_result_text(pCtx, result.c_str(), -1, SQLITE_TRANSIENT);
+}
+
+static void jieba_query(sqlite3_context* pCtx,
+                        int nVal,
+                        sqlite3_value** apVal) {
+    if (nVal >= 1) {
+        const char* text = (const char*)sqlite3_value_text(apVal[0]);
+        if (text) {
+            int flags = 1;
+            if (nVal >= 2) {
+                flags = atoi((const char*)sqlite3_value_text(apVal[1]));
+            }
+            std::string result =
+                simple_tokenizer::SimpleTokenizer::tokenize_jieba_query(
+                    text, std::strlen(text), flags);
+            sqlite3_result_text(pCtx, result.c_str(), -1, SQLITE_TRANSIENT);
+            return;
+        }
+    }
+    sqlite3_result_null(pCtx);
 }
 #endif
 
-static void simple_query(sqlite3_context *pCtx, int nVal, sqlite3_value **apVal) {
-  int rc;
-  if (nVal >= 1) {
-    const char *text = (const char *)sqlite3_value_text(apVal[0]);
-    if (text) {
-      int flags = 1;
-      if (nVal >= 2) {
-        flags = atoi((const char *)sqlite3_value_text(apVal[1]));
-      }
-      std::string result = simple_tokenizer::SimpleTokenizer::tokenize_query(text, std::strlen(text), flags);
-      sqlite3_result_text(pCtx, result.c_str(), -1, SQLITE_TRANSIENT);
-      return;
+static void simple_query(sqlite3_context* pCtx,
+                         int nVal,
+                         sqlite3_value** apVal) {
+    if (nVal >= 1) {
+        const char* text = (const char*)sqlite3_value_text(apVal[0]);
+        if (text) {
+            int flags = 1;
+            if (nVal >= 2) {
+                flags = atoi((const char*)sqlite3_value_text(apVal[1]));
+            }
+            std::string result =
+                simple_tokenizer::SimpleTokenizer::tokenize_query(
+                    text, std::strlen(text), flags);
+            sqlite3_result_text(pCtx, result.c_str(), -1, SQLITE_TRANSIENT);
+            return;
+        }
     }
-  }
-  sqlite3_result_null(pCtx);
+    sqlite3_result_null(pCtx);
 }
 
-int sqlite3_simple_init(sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *pApi) {
-  int rc = SQLITE_OK;
-  SQLITE_EXTENSION_INIT2(pApi);
+int sqlite3_simple_init(sqlite3* db,
+                        char** pzErrMsg,
+                        const sqlite3_api_routines* pApi) {
+    int rc = SQLITE_OK;
+    SQLITE_EXTENSION_INIT2(pApi);
 
-  rc = sqlite3_create_function(db, "simple_query", -1, SQLITE_UTF8 | SQLITE_DETERMINISTIC, NULL, &simple_query, NULL,
-                               NULL);
+    rc = sqlite3_create_function(db, "query", -1,
+                                 SQLITE_UTF8 | SQLITE_DETERMINISTIC, NULL,
+                                 &query, NULL, NULL);
+    if (rc != SQLITE_OK)
+        return rc;
+
+    rc = sqlite3_create_function(db, "simple_query", -1,
+                                 SQLITE_UTF8 | SQLITE_DETERMINISTIC, NULL,
+                                 &simple_query, NULL, NULL);
+    if (rc != SQLITE_OK)
+        return rc;
+
 #ifdef USE_JIEBA
-  rc = sqlite3_create_function(db, "jieba_query", -1, SQLITE_UTF8 | SQLITE_DETERMINISTIC, NULL, &jieba_query, NULL,
-                               NULL);
-  rc = sqlite3_create_function(db, "jieba_dict", 1, SQLITE_UTF8 | SQLITE_DETERMINISTIC, NULL, &jieba_dict, NULL, NULL);
+    rc = sqlite3_create_function(db, "jieba_query", -1,
+                                 SQLITE_UTF8 | SQLITE_DETERMINISTIC, NULL,
+                                 &jieba_query, NULL, NULL);
+    if (rc != SQLITE_OK)
+        return rc;
+
+    rc = sqlite3_create_function(db, "jieba_dict", 1,
+                                 SQLITE_UTF8 | SQLITE_DETERMINISTIC, NULL,
+                                 &jieba_dict, NULL, NULL);
+    if (rc != SQLITE_OK)
+        return rc;
 #endif
 
-  // fts5_tokenizer tokenizer = {fts5AsciiCreate, fts5AsciiDelete, fts5AsciiTokenize };
-  fts5_tokenizer tokenizer = {fts5_simple_xCreate, fts5_simple_xDelete, fts5_simple_xTokenize};
-  fts5_api *fts5api;
-  rc = fts5_api_from_db(db, &fts5api);
-  if (rc != SQLITE_OK) return rc;
-  if (fts5api == 0 || fts5api->iVersion < 2) {
-    return SQLITE_ERROR;
-  }
-  rc = fts5api->xCreateTokenizer(fts5api, "simple", reinterpret_cast<void *>(fts5api), &tokenizer, NULL);
-  rc =
-      fts5api->xCreateFunction(fts5api, "simple_highlight", reinterpret_cast<void *>(fts5api), &simple_highlight, NULL);
-  rc = fts5api->xCreateFunction(fts5api, "simple_highlight_pos", reinterpret_cast<void *>(fts5api),
-                                &simple_highlight_pos, NULL);
-  rc = fts5api->xCreateFunction(fts5api, "simple_snippet", reinterpret_cast<void *>(fts5api), &simple_snippet, NULL);
-  return rc;
+    // fts5_tokenizer tokenizer = {fts5AsciiCreate, fts5AsciiDelete,
+    // fts5AsciiTokenize };
+    fts5_tokenizer tokenizer = {fts5_simple_xCreate, fts5_simple_xDelete,
+                                fts5_simple_xTokenize};
+    fts5_api* fts5api;
+    rc = fts5_api_from_db(db, &fts5api);
+    if (rc != SQLITE_OK)
+        return rc;
+    if (fts5api == 0 || fts5api->iVersion < 2) {
+        return SQLITE_ERROR;
+    }
+    rc = fts5api->xCreateTokenizer(
+        fts5api, "simple", reinterpret_cast<void*>(fts5api), &tokenizer, NULL);
+    rc = fts5api->xCreateFunction(fts5api, "simple_highlight",
+                                  reinterpret_cast<void*>(fts5api),
+                                  &simple_highlight, NULL);
+    rc = fts5api->xCreateFunction(fts5api, "simple_highlight_pos",
+                                  reinterpret_cast<void*>(fts5api),
+                                  &simple_highlight_pos, NULL);
+    rc = fts5api->xCreateFunction(fts5api, "simple_snippet",
+                                  reinterpret_cast<void*>(fts5api),
+                                  &simple_snippet, NULL);
+    return rc;
 }
